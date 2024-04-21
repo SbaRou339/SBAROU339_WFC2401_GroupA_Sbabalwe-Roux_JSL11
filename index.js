07f263cc5abee27fdbee3eabdc0618418f51f4ef
SbabalweRoux <sbaviaroux@gmail.com>
2024-04-21 22:28:37 +0200

    Corrected ternary operator syntax fetchAndDisplayBoardsAndTasks()

--- index.js
+++ index.js
@@ -32,7 +32,7 @@ function fetchAndDisplayBoardsAndTasks() {
   displayBoards(boards);
   if (boards.length > 0) {
     const localStorageBoard = JSON.parse(localStorage.getItem("activeBoard"))
-    activeBoard = localStorageBoard ? localStorageBoard ;  boards[0]; 
+    activeBoard = localStorageBoard ? localStorageBoard :  boards[0];  // Corrected ternary operator syntax
     elements.headerBoardName.textContent = activeBoard
     styleActiveBoard(activeBoard)
     refreshTasksUI();
